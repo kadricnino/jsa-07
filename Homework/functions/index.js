a159e0daac531c2c8f76c47725d73fc68f5a3934
kadricnino <58231210+kadricnino@users.noreply.github.com>
2020-09-22 11:44:36 +0200

    Add files via upload

---
+++ Homework/functions/index.js
@@ -0,0 +1,69 @@
+function toMiles(km) {
+    return mi=km*0.62137;
+};
+toMiles(27);
+console.log(mi);
+
+
+function toPounds(kg) {
+    return lb=kg*2.2046;
+};
+toPounds(105);
+console.log(lb);
+
+
+function toGallon(l) {
+    return gallons=l*0.26417;
+};
+toGallon(26);
+console.log(gallons)
+
+
+function toFeet(m) {
+    return ft=m*3.2808;
+};
+toFeet(1500);
+console.log(ft);
+
+
+function toKnots(kph) {
+    return knots=kph/1.852;
+};
+toKnots(250);
+console.log(knots);
+
+
+
+// const KilometersToMiles = (km) => {
+//     return mi=km*0.62137;
+// };
+// KilometersToMiles(21);
+// console.log(mi);
+
+
+// const KilogramsToPounds = (kg) => {
+//     return lb=kg*2.2046;
+// };
+// KilogramsToPounds(89)
+// console.log(lb);
+
+
+// const LitersToGallons = (l) => {
+//     return gallons=l*0.26417;
+// };
+// LitersToGallons(18);
+// console.log(gallons);
+
+
+// const MetersToFeet = (m) => {
+//     return ft=m*3.2808;
+// };
+// MetersToFeet(2700);
+// console.log(ft);
+
+
+// const KphToKnots = (kph) => {
+//     return knots=kph/1.852;
+// };
+// KphToKnots(333);
+// console.log(knots);
